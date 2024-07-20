74a7b05c2479ac1104a23428948e43191932ef2c
John Doe <youssifgamal123@gmail.com>
2024-07-20 04:18:35 +0300

    ssd

--- booky/lib/core/ssx.dart
+++ booky/lib/core/ssx.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class stdf extends StatelessWidget {
-  const stdf({super.key});
+class Ssx extends StatelessWidget {
+  const Ssx({super.key});
 
   @override
   Widget build(BuildContext context) {
